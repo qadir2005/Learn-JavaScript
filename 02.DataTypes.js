f98c615985361905e7a5baef4ba869cc056a6568
Abdeul Qadir <qadir20005@gmail.com>
2025-03-17 17:28:30 +0500

    some changing

---
+++ 02.DataTypes.js
@@ -0,0 +1,4 @@
+let age = 18
+let prizeBond = 51156563145645454654
+let Name = "Abdul Qadir"
+
---
+++ stringConvertion.js
@@ -0,0 +1,6 @@
+// ====================== Operations ======================
+
+let String1= "Abdul "
+let String2= " Qadir"
+
+console.log(String1+String2);
