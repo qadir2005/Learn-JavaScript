06b390a61c5042f2c7180686bd4fd8e6fedd573e
Abdeul Qadir <qadir20005@gmail.com>
2025-03-24 18:09:51 +0500

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,45 @@
+// Datatype ki kitne categories hai 
+//  jis bases pr data ko access aur save kia jata hai us basis par 2 categories hai 
+// 1 primitive Datatype
+//  non Primitive Datatype
+
+// Primitive
+        // Data Types
+                // type : Stirng
+                // number
+                // null
+                // undefined
+                // symbol
+                // Boolean
+                // Big Int
+
+
+                const score = 500;
+                const updatedScore = 500.32;
+                
+                const userName = "Abdul Qadir";
+                const isLoggin = true;
+                
+                const unique = Symbol("1234")
+                const matchNuiqe = Symbol("1234")
+                
+                const bigInt = 546513654465463156514654165465441651
+                
+                let newundefined;
+                
+                console.log(unique===matchNuiqe);
+                       
+
+// Reference / Non Premitive Data Types
+        // Data Types
+                // Array
+                // Object
+                // function
+
+let array = [ "apple", "orange", "mango"]
+let object = {
+    className : 10,
+    SchoolName: "blue Moon",
+    StudnetName : "Abdul Qadir"
+
+}
